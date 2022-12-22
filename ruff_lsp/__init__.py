b27f13b9c5679b1edd8ea1cf215d516a5da9714f
yaegassy <yaegassy@users.noreply.github.com>
2022-12-22 14:50:33 +0900

    fix: support for python 3.7

--- ruff_lsp/__init__.py
+++ ruff_lsp/__init__.py
@@ -1,3 +1,8 @@
-from importlib.metadata import version
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
 
 __version__ = version("ruff-lsp")
