c2d3451eec81074c3fa708b709c2c98311281e3d
yaegassy <yaegassy@users.noreply.github.com>
2022-12-23 00:33:45 +0900

    feat: add --version option (#10)

---
+++ ruff_lsp/__init__.py
@@ -0,0 +1,8 @@
+import sys
+
+if sys.version_info < (3, 8):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+
+__version__ = version("ruff-lsp")
--- ruff_lsp/__main__.py
+++ ruff_lsp/__main__.py
@@ -1,7 +1,24 @@
+import argparse
+import sys
+
+from ruff_lsp import __version__
+
+
 def main() -> None:
     from ruff_lsp.server import LSP_SERVER
 
-    LSP_SERVER.start_io()
+    parser = argparse.ArgumentParser(prog="ruff-lsp")
+    parser.add_argument(
+        "--version",
+        help="display version information and exit",
+        action="store_true",
+    )
+    args = parser.parse_args()
+    if args.version:
+        print(__version__)
+        sys.exit(0)
+    else:
+        LSP_SERVER.start_io()
 
 
 if __name__ == "__main__":
